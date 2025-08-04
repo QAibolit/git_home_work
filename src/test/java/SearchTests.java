940f57fd598b86f6f31e822b28494ad0d05a7605
Dina <sd@myexe.net>
2025-08-04 19:13:18 +0300

    Add test number

--- src/test/java/SearchTests.java
+++ src/test/java/SearchTests.java
@@ -6,9 +6,9 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
 
-
     @Test
     void captchaTest() {
+        System.out.println("Test number: 1");
         open("https://www.google.com/");
         $("[name=q]").setValue("selenide").pressEnter();
         $("html").shouldHave(text("Об этой странице"));
